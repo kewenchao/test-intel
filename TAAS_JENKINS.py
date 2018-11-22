1f1b1563124d1af6df8946a92572da43d0ae7268
kewenchx <wenchao.ke@intel.com>
2018-11-22 13:12:55 +0800

    ha ha
    Signed-off-by: kewenchx <wenchao.ke@intel.com>

--- TAAS_JENKINS.py
+++ TAAS_JENKINS.py
@@ -25,9 +25,11 @@ def submit_sessions(**kwargs):
     job_submit_url = urljoin(
         job_request['server_url'],
         'build' if kwargs is None else 'buildWithParameters')
+    status = True
+    message = "success"
     try:
         jks_server = 'jf1atjenkins.ostc.intel.com'
-        res = JENKINS_JOB_API.findall(job_submit_url)
+        res = JENKINS_JOB_API.findall(kwargs['job_submit_url'])
         if res:
             jks_server = res[0]
         crumb_url = JENKINS_CRUMB_API.format(kwargs['username'], kwargs['password'], jks_server)
@@ -35,20 +37,19 @@ def submit_sessions(**kwargs):
         r.raise_for_status()
         session.headers.update({'Jenkins-Crumb': r.content.split(':')[1]})
     except RequestException as exc:
-        log.error('Failed to get token for %s : %s' % (kwargs['username'], exc.message))
-
+        status = False
+        message = 'Failed to get token for %s : %s' % (kwargs['username'], exc.message)
     try:
         r = session.post(url=job_submit_url, params=kwargs, auth=job_auth)
         r.raise_for_status()
     except RequestException as exc:
-        log.error('Failed to trigger a Jenkins build: %s', exc.message)
-
+        status = False
+        message = 'Failed to trigger a Jenkins build:{}'.format(exc.message)
     # Polling the build request till the build starts.
     job_request_url = r.headers.get('Location')
     if job_request_url is None:
-        log.error(
-            'Cannot get build request information: the "Location" header is '
-            'missing from Jenkins HTTP response: %r', r.content)
+        status = False
+        message = 'Cannot get build request information: the "Location" header is missing from Jenkins HTTP response: {}"'.format(r.content) 
 
     job_request_url = urljoin(job_request_url, 'api', 'json')
     log.info('Polling queued task at: %r', job_request_url)
@@ -60,27 +61,30 @@ def submit_sessions(**kwargs):
             r = session.get(job_request_url)
             r.raise_for_status()
         except RequestException as exc:
-            log.error(exc.message)
+            status = False
+            message = exc.message
         try:
             jenkins_task = r.json()
         except ValueError:
-            log.error('Cannot decode JSON response: %r', r.content)
+            status = False
+            message = 'Cannot decode JSON response:{}'.format(r.content)
         log.debug('Jenkins task: %r', jenkins_task)
         if 'task' not in jenkins_task:
-            log.error(
-                'The object pointed to at %r does not look like a valid task, '
-                'aborting: %r', job_request_url, jenkins_task)
+            status = False
+            message = 'The object pointed to at %r does not look like a valid task, aborting: %r' % (job_request_url, jenkins_task)
         try:
             job_request['job_url'] = jenkins_task['executable']['url']
         except TypeError as exc:
             try:
                 if jenkins_task.get('cancelled', False):
-                    log.error('The task has been cancelled.')
+                    status = False
+                    message = 'The task has been cancelled.'
             except AttributeError:
                 # We definitely are in bad luck!
-                log.error('Invalid task object from Jenkins: %r', jenkins_task)
+                status = False
+                message = 'Invalid task object from Jenkins:{}'.format(jenkins_task)
             finally:
-                log.error('Unable to get build URL')
+                message ='Unable to get build URL'
         except KeyError:
             backoff = min(kwargs['max_backoff'], backoff + (backoff // 2) + 1)
             sleep_time += backoff
@@ -88,11 +92,12 @@ def submit_sessions(**kwargs):
                 log.info('Waiting for Jenkins build to start; sleep %s', backoff)
                 sleep(backoff)
             else:
-                log.error('Out of max sleep time to wait the response from jenkins and return the server url instead')
+                status = False
+                message = 'Out of max sleep time to wait the response from jenkins and return the server url instead'
                 job_request['job_url'] = job_request['server_url']
                 break
         else:
             json.dumpfile(job_request, kwargs['job_request_pname'])
-
-
+    job_url = job_request['job_url']
+    return status, message, job_url
 
